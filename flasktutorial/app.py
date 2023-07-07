dd3e87bb074eae275c3adddfd78494e643bdb9f1
raghuprasadks <prasadraghuks@gmail.com>
2023-07-07 12:12:08 +0530

    updated

--- flasktutorial/app.py
+++ flasktutorial/app.py
@@ -1,13 +1,22 @@
 from flask import Flask
 print("__name__ ",__name__)
 app = Flask(__name__)
-
+#http://127.0.0.1:5000/
 @app.route("/")
 def welcome():
     return "Welcome to Flask"
+#http://127.0.0.1:5000/greet
 @app.route("/greet")
 def greet():
     return "Greetings from Flask "
+#http://127.0.0.1:5000/greetinperson/raghu
+@app.route("/greetinperson/<string:name>")
+def personalizedgreeting(name):
+    return f" {name} Greetings from Flask "
+#http://127.0.0.1:5000/add/10/20
+@app.route("/add/<int:num1>/<int:num2>")
+def add(num1,num2):
+    return str(num1+num2)
 
 if(__name__=="__main__"):
     app.run(debug=True)
