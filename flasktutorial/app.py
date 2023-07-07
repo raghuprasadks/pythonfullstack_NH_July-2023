c25d1cbc04eef34fab4b6eee9b3a1287a2712a35
raghuprasadks <prasadraghuks@gmail.com>
2023-07-07 12:30:05 +0530

    updated

--- flasktutorial/app.py
+++ flasktutorial/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+#from flask import Flask
+from flask import Flask, request, jsonify
 print("__name__ ",__name__)
 app = Flask(__name__)
 #http://127.0.0.1:5000/
@@ -9,14 +10,22 @@ def welcome():
 @app.route("/greet")
 def greet():
     return "Greetings from Flask "
+
 #http://127.0.0.1:5000/greetinperson/raghu
 @app.route("/greetinperson/<string:name>")
 def personalizedgreeting(name):
     return f" {name} Greetings from Flask "
+
 #http://127.0.0.1:5000/add/10/20
 @app.route("/add/<int:num1>/<int:num2>")
 def add(num1,num2):
     return str(num1+num2)
+
+@app.route("/users/add",methods=["post"])
+def addUser():
+    user = request.get_json()
+    print("post method")
+    return user
 
 if(__name__=="__main__"):
     app.run(debug=True)
